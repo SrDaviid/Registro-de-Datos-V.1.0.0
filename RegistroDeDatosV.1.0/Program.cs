0b6f433769989ec512082e1b3089c4fafa9833e4
SrDaviid <70109934+SrDaviid@users.noreply.github.com>
2020-09-11 16:06:50 -0700

    Add files via upload

---
+++ RegistroDeDatosV.1.0/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RegistroDeDatosV._1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "filename.csv";
+
+            using (StreamWriter sw = File.AppendText (filename))
+            {
+                sw.WriteLine("ID, Name, Last Name, Age");
+
+                Console.WriteLine("Cual es tu cedula?");
+                int cedula = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Cual es tu nombre?");
+                string nombre = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("Cual es tu apellido?");
+                string apellido = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("Cual es tu edad");
+                int edad = Convert.ToInt32(Console.ReadLine());
+
+                sw.WriteLine(+ cedula +" , "+ nombre +" , "+ apellido +" , "+ edad);
+
+            }     
+                  
+        }
+       
+    }
+}
